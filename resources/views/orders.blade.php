298401d475e11572c1954dc20e8882722dc15395
ErkkiTa <erkkitamb5@gmail.com>
2020-10-06 15:28:35 +0300

    tsau

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,12 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tellimused</title>
-</head>
-<body>
+@extends("layout.index")
+@section("content")
 
-</body>
-</html>
+<table class="w-full table-auto">
+        <thead class="font-bold">
+            <tr>
+                <td class="px-4 py-2"><a href="?sort=delivery_address&order={{ $orders }}">Tarne address</a></td>
+                <td class="px-4 py-2 w-32"><a href="?sort=order_date&order={{ $orders }}">Tellimuse kuupäev</a></td>
+                <td class="px-4 py-2 w-32"><a href="?sort=status&order={{ $orders }}">Staatus</a></td>
+                <td class="px-4 py-2 w-32"><a href="?sort=client_id&order={{ $orders }}">Kliendi Id</a></td>
+                <td class="px-4 py-2 w-32"><a href="?sort=book_id&order={{ $orders }}">Raamatu id</a></td>
+            </tr>
+        </thead>
+        <tbody class="text-sm">
+            @foreach($orders ?? '' as $book)
+            <tr>
+                <td class="border rounded px-4 py-2"><a href="/books/{{ $book->id }}">{{ $book->title }}</a></td>
+                <td class="border rounded px-4 py-2">{{ $book->delivery_address }}</td>
+                <td class="border rounded px-4 py-2">{{ $book->pages }}</td>
+                <td class="border rounded px-4 py-2">{{ $book->price }}</td>
+                <td class="border rounded px-4 py-2">{{ $book->book_id }}</td>
+
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+
+
+@endsection
