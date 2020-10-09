9d3e6d867fd556c71291523bed4b3a854e987d98
ErkkiTa <44016792+ErkkiTa@users.noreply.github.com>
2020-10-09 09:35:40 +0300

    Update Order.php

--- app/Order.php
+++ app/Order.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    public function scopeSort($query, $order)
+    {
+        if (request()->has('sort')) {
+            return $query->orderBy(request()->query('sort'), $order);
+        }
+        return $query;
+    }
+
+    public function book() {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function client() {
+        return $this->belongsTo(Client::class);
+    }
 }
+
